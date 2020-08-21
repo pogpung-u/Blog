6839529fc074a6c7f2a4045390e6fc21abd41ac7
Admin <blog.on.django@gmail.com>
2020-08-21 10:59:01 +0300

    2 commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const path = require('path')
+const mongoose = require('mongoose')
+const keys = require('./keys')
 
 const port = process.env.PORT || 5000
 const clientPath = path.join(__dirname, 'client')
 
 const app = express()
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB connected.'))
+    .catch(err => console.error(err))
+
 app.use(express.static(clientPath))
 
 app.listen(port, () => {
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://liryon:420296Qwe@cluster0.whnpn.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
