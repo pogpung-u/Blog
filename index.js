6c1be5068126b1b408a93733955bd1da98d63f54
Admin <blog.on.django@gmail.com>
2020-08-21 11:32:05 +0300

    routing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require('path')
 const mongoose = require('mongoose')
+const postRouter = require('./routes/post')
 const keys = require('./keys')
 
 const port = process.env.PORT || 5000
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI)
     .catch(err => console.error(err))
 
 app.use(express.static(clientPath))
+app.use('/api/post', postRouter)
 
 app.listen(port, () => {
     console.log(`Server has been started on port ${port}`)
